d611458a9baf8f5d7461f04d88fec57516dfaad3
malcoscos <so12ma131999@gmail.com>
2023-10-31 13:17:28 +0000

    osbook_day05c

--- kernel/.font.d
+++ kernel/.font.d
@@ -1,1 +1,5 @@
-font.o: font.cpp font.hpp graphics.hpp frame_buffer_config.hpp
+font.o: font.cpp font.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
--- kernel/.main.d
+++ kernel/.main.d
@@ -2,6 +2,7 @@ main.o: main.cpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/version \
-  frame_buffer_config.hpp graphics.hpp font.hpp \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h
+  frame_buffer_config.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp font.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint
