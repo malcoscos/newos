812d44dd1deddd62911d3a66fc9d8b9339ef3560
malcoscos <so12ma131999@gmail.com>
2023-11-04 01:25:04 +0000

    osbook_day06a

--- kernel/graphics.cpp
+++ kernel/graphics.cpp
@@ -4,7 +4,6 @@
  * 画像描画関連のプログラムを集めたファイル．
  */
 
-// #@@range_begin(pixel_writer_impl)
 #include "graphics.hpp"
 
 void RGBResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
@@ -13,11 +12,31 @@ void RGBResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
   p[1] = c.g;
   p[2] = c.b;
 }
-// #@@range_end(pixel_writer_impl)
 
 void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
   auto p = PixelAt(x, y);
   p[0] = c.b;
   p[1] = c.g;
   p[2] = c.r;
+}
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c) {
+  for (int dx = 0; dx < size.x; ++dx) {
+    writer.Write(pos.x + dx, pos.y, c);
+    writer.Write(pos.x + dx, pos.y + size.y - 1, c);
+  }
+  for (int dy = 1; dy < size.y - 1; ++dy) {
+    writer.Write(pos.x, pos.y + dy, c);
+    writer.Write(pos.x + size.x - 1, pos.y + dy, c);
+  }
+}
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c) {
+  for (int dy = 0; dy < size.y; ++dy) {
+    for (int dx = 0; dx < size.x; ++dx) {
+      writer.Write(pos.x + dx, pos.y + dy, c);
+    }
+  }
 }
