05f0b5b48c59d35f6a513f769081de0515ee0bdf
malcoscos <so12ma131999@gmail.com>
2023-12-11 13:44:07 +0000

    osbook_day10d

--- kernel/.console.d
+++ kernel/.console.d
@@ -1,10 +1,6 @@
-console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/string.h font.hpp \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint layer.hpp \
+console.o: console.cpp console.hpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/memory \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/version \
@@ -12,10 +8,14 @@ console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/exception \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/new \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/utility \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/limits \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
@@ -26,20 +26,21 @@ console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
   /home/vscode/osbook/devenv/x86_64-elf/include/assert.h \
-  /home/vscode/osbook/devenv/x86_64-elf/include/_ansi.h \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/map \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
+  /home/vscode/osbook/devenv/x86_64-elf/include/_ansi.h graphics.hpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/functional \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/bit \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/optional \
+  frame_buffer_config.hpp window.hpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/vector \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__bit_reference \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/climits \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/limits.h \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__split_buffer \
-  window.hpp frame_buffer.hpp error.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/optional \
+  frame_buffer.hpp error.hpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/array
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/array font.hpp \
+  layer.hpp /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/map \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle
