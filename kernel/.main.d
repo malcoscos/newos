bbe4560b9352c6077ea069e00ad057772057227e
malcoscos <so12ma131999@gmail.com>
2023-12-16 13:00:11 +0000

    osbook_day11d

--- kernel/.main.d
+++ kernel/.main.d
@@ -53,4 +53,4 @@ main.o: main.cpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/map \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
-  timer.hpp
+  timer.hpp /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/queue
