8235089767652491b3a8a7699e553a0d2a8275a8
malcoscos <so12ma131999@gmail.com>
2023-12-24 10:37:43 +0000

    osbook_day12b

--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -46,5 +46,7 @@ class TimerManager {
 // #@@range_end(timermgr)
 
 extern TimerManager* timer_manager;
+extern unsigned long lapic_timer_freq;
+const int kTimerFreq = 100;
 
 void LAPICTimerOnInterrupt();
