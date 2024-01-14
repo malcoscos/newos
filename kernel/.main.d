d2b9077823614829a5d792ebe00d4279de1161d0
malcoscos <soma.nakagawa.77@gmail.com>
2024-01-14 01:06:11 +0000

    osbook_day17b

--- kernel/.main.d
+++ kernel/.main.d
@@ -62,4 +62,4 @@ main.o: main.cpp \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
   /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
   timer.hpp /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/queue \
-  acpi.hpp keyboard.hpp task.hpp terminal.hpp
+  acpi.hpp keyboard.hpp task.hpp terminal.hpp fat.hpp
